cd4a843889313c901d7c50131cf2fb9fb5521fb8
@vishwajeetsharma09 <2000031822@kluniversity.in>
2023-10-31 19:26:00 +0530

    portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Vishwajeet Sharma</span> Greetings and welcome to my portfolio! I am Vishwajeet Sharma, a professional with a deep-rooted passion for logical problem-solving, software engineering, and full-stack development. Over the course of my career, I have seamlessly integrated creativity with a structured approach to consistently deliver innovative solutions that not only meet set objectives but also challenge the conventional norms.        My expertise spans across multiple domains, including Data Structures and Algorithms (DSA), software engineering, and full-stack development. By infusing my work with a logical mindset, I have successfully tackled intricate challenges, leveraging my analytical prowess to devise comprehensive and effective strategies."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
